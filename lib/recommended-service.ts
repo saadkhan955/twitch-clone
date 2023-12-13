16213875cdef9f222f8fcb7d596a03404c13690a
Saad Khan <saad.khan955@gmail.com>
2023-12-13 21:57:22 +0530

    Created Recommended List

---
+++ lib/recommended-service.ts
@@ -0,0 +1,13 @@
+import { db } from '@/lib/db'
+import { getSelf } from '@/lib/auth-service'
+
+
+export const getRecommended = async () => {
+  const user = await db.user.findMany({
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
+  return user;
+}
