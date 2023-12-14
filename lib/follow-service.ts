ee33ca97dbdd5b698e9995c3ec53260bf078a8ee
Saad Khan <saad.khan955@gmail.com>
2023-12-14 19:24:48 +0530

    Follow service added

---
+++ lib/follow-service.ts
@@ -0,0 +1,106 @@
+import { db } from "@/lib/db"
+import { getSelf } from "@/lib/auth-service"
+
+export const isFollowingUser = async (id: string) => {
+  try {
+    const self = await getSelf();
+
+    const otherUser = await db.user.findUnique({
+      where: { id }
+    })
+
+    if (!otherUser) {
+      throw new Error("User not found");
+    }
+
+    if (otherUser.id === self.id) {
+      return true;
+    }
+
+    const existingFollow = await db.follow.findFirst({
+      where: {
+        followerId: self.id,
+        followingId: otherUser.id,
+      }
+    })
+
+    return !!existingFollow;
+
+  } catch {
+    return false;
+  }
+}
+
+export const followUser = async (id?: string) => {
+  const self = await getSelf();
+
+  const otherUser = await db.user.findUnique({
+    where: { id }
+  })
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  if (otherUser.id === self.id) {
+    throw new Error("You cannot follow yourself");
+  }
+
+  const existingFollow = await db.follow.findFirst({
+    where: {
+      followerId: self.id,
+      followingId: otherUser.id,
+    }
+  })
+  if (existingFollow) {
+    throw new Error("You are already following this user");
+  }
+
+  const follow = await db.follow.create({
+    data: {
+      followerId: self.id,
+      followingId: otherUser.id,
+    },
+    include: {
+      following: true,
+      follower: true,
+    }
+  })
+  return follow;
+}
+
+export const unfollowUser = async (id?: string) => {
+  const self = await getSelf();
+
+  const otherUser = await db.user.findUnique({
+    where: { id }
+  })
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  if (otherUser.id === self.id) {
+    throw new Error("You cannot unfollow yourself");
+  }
+
+  const existingFollow = await db.follow.findFirst({
+    where: {
+      followerId: self.id,
+      followingId: otherUser.id,
+    }
+  })
+  if (!existingFollow) {
+    throw new Error("You are not following this user");
+  }
+
+  const follow = await db.follow.delete({
+    where: {
+      id: existingFollow.id,
+    },
+    include: {
+      following: true,
+    }
+  })
+  return follow;
+}
