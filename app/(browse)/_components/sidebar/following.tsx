7089151943555b68a123de433fe795a2cb82d865
Saad Khan <saad.khan955@gmail.com>
2024-01-04 00:31:44 +0530

    Video Component Added

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Follow, Stream, User } from "@prisma/client"
+import { Follow, User } from "@prisma/client"
 
 import { useSidebar } from "@/store/use-sidebar"
 import { UserItem, UserItemSkeleton } from "./user-item"
@@ -8,7 +8,7 @@ import { UserItem, UserItemSkeleton } from "./user-item"
 interface FollowingProps {
   data: (Follow & {
     following: User & {
-      stream: Stream | null
+      stream: { isLive: boolean } | null
     }
   })[]
 }
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -4,8 +4,20 @@ export const getUserByUsername = async (username: string) => {
   const user = await db.user.findUnique({
     where: {
       username,
+    },
+    include: {
+      stream: true
     }
   })
+  return user;
+}
 
+export const getUserByID = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      stream: true
+    }
+  })
   return user;
 }
